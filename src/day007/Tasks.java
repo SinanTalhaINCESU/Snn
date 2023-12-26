a955c0c401a0963080fb18efcbec6d8b5b814ec4
sinantalhaincesu <snn99tlh@hotmail.com>
2023-12-26 21:38:40 +0300

    Snn has been changed again

---
+++ src/day007/Tasks.java
@@ -0,0 +1,15 @@
+package day007;
+
+public class Tasks {
+    public static void main(String[] args) {
+
+        // Benim adım ... ve ... yaşındayım.
+        // Önce bu şekilde daha sonra isim ve yaşı eğiştirip yazalım.
+
+        String ad = "Talha";
+        int yas = 25;
+
+        System.out.println("Benim adım Sinan ve 24 yaşındayım.");
+        System.out.println("Benim adım " + ad + " ve " + yas + " yaşındayım.");
+    }
+}
