c502675416ef6dd38ada0a72fd06ed9ecd9b2b22
sinantalhaincesu <snn99tlh@hotmail.com>
2023-12-26 20:40:15 +0300

    Snn has been created again

---
+++ src/day005/Triangle.java
@@ -0,0 +1,13 @@
+package day005;
+
+public class Triangle {
+    public static void main(String[] args) {
+        //Task: Yıldızlardan oluşan bir üçgen çıktısı veren bir java programı yazın.
+
+        System.out.println("    *    ");
+        System.out.println("   * *   ");
+        System.out.println("  *   *  ");
+        System.out.println(" *     * ");
+        System.out.println("*********");
+    }
+}
