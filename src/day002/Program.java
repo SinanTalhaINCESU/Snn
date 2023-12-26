65d221b25eca0c5c51b6a68866de1b85d99f5e58
sinantalhaincesu <snn99tlh@hotmail.com>
2023-12-26 20:39:34 +0300

    Snn has been created again

---
+++ src/day002/Program.java
@@ -0,0 +1,7 @@
+package day002;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Merhaba!!");
+    }
+}
---
+++ src/day004/Program.java
@@ -0,0 +1,12 @@
+package day004;
+
+public class Program {
+    public static void main(String[] args) {
+        // Task: Yıldızlardan oluşan bir kare çıktısı veren bir java programı yazınız.
+
+        System.out.println("*******");
+        System.out.println("*******");
+        System.out.println("*******");
+
+    }
+}
