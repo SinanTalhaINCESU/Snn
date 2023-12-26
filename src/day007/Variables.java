756b0e33e7a8682a2d4c34e7736bf2ed24c47042
sinantalhaincesu <snn99tlh@hotmail.com>
2023-12-26 20:41:03 +0300

    Snn has been created again

---
+++ src/day007/Variables.java
@@ -0,0 +1,11 @@
+package day007;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        //karenin alalı ve çevresini hesaplayınız.
+        int kenar = 5;
+        System.out.println(kenar*kenar);
+        System.out.println(kenar*4);
+    }
+}
