c6b12caf4019c5d12e99f8932fffb525daee174f
sinantalhaincesu <snn99tlh@hotmail.com>
2023-12-26 20:42:35 +0300

    Snn has been created again and again.

--- src/day007/Variables.java
+++ src/day007/Variables.java
@@ -4,6 +4,7 @@ public class Variables {
     public static void main(String[] args) {
 
         //karenin alalı ve çevresini hesaplayınız.
+
         int kenar = 5;
         System.out.println(kenar*kenar);
         System.out.println(kenar*4);
