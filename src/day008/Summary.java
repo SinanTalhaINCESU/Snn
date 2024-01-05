6f4e323bbde2a80f720c6de2a767a7398555f64a
sinantalhaincesu <snn99tlh@hotmail.com>
2024-01-05 19:25:24 +0300

    Snn has been changed, day 8

---
+++ src/day008/Summary.java
@@ -0,0 +1,38 @@
+package day008;
+
+public class Summary {
+    public static void main(String[] args) {
+        System.out.println("Merhaba!");
+
+        int sayi=5;
+
+        char c='A';
+
+        double kutununAgirligi=3.0;
+
+        float f=3.0F;
+
+
+        String adi="Ahmet";
+        String soyadi="Dursun";
+
+        String isim=adi+" "+soyadi;
+
+        System.out.println(isim);
+
+
+        boolean resut1=true;
+        boolean resut2=false;
+        boolean resut3=6<3;
+
+        String str="23"+18;
+
+        System.out.println(str);
+
+
+
+        String mesaj="Çocuklar, hep bir ağızdan \"Neden biz susmak zorundayız?\" dediler";
+        System.out.println(mesaj);
+
+    }
+}
