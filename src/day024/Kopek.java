dbfb297c43e4df5b077f745d29237535869580e5
sinantalhaincesu <snn99tlh@hotmail.com>
2024-01-22 17:37:58 +0300

    Snn has been changed, day 24

---
+++ src/day024/Kopek.java
@@ -0,0 +1,15 @@
+package day024;
+
+public class Kopek {
+
+    public String cinsi;
+
+    public int yasi;
+
+    double agirligi;
+
+
+    public void bilgiGoster(){
+        System.out.printf("%s köpeği [%d - %5.1f Kg.]",cinsi,yasi,agirligi);
+    }
+}
