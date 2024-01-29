df050e7881294d9e0a0e268af0b61b32d6551d7b
sinantalhaincesu <snn99tlh@hotmail.com>
2024-01-29 21:50:31 +0300

    Snn has been changed, day 29

---
+++ src/day029/Koyun.java
@@ -0,0 +1,13 @@
+package day029;
+
+public class Koyun extends Hayvan{
+
+    public Koyun(){
+        System.out.println("Koyun nesnesi oluşturuldu.");
+    }
+
+    @Override
+    public void beslen() {
+        System.out.println("Koyun ot ile besleniyor.");
+    }
+}
---
+++ src/day029/MainApp2.java
@@ -0,0 +1,21 @@
+package day029;
+
+public class MainApp2 {
+    public static void main(String[] args) {
+        //Kural: Bir ata sınıf, kendisinden türetilmiş alt sınıf örneklerine referans olabilir.
+        Object obj1=new Hayvan();
+        Hayvan hy1=new Kedi();
+        System.out.println();
+        Hayvan hy2=new Koyun();
+        System.out.println();
+        Hayvan hy3=new Hayvan();
+        Object obj2=new Kedi();
+
+        System.out.println();
+        hy1.beslen();
+        System.out.println();
+        hy2.beslen();
+        System.out.println();
+        hy3.beslen();
+    }
+}
