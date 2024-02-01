9dd9e1875238317efe1a5e8dd3eea6d9074f01b3
sinantalhaincesu <snn99tlh@hotmail.com>
2024-02-01 19:06:35 +0300

    Snn has been changed, day 30v2

---
+++ src/day030/Kitap.java
@@ -0,0 +1,61 @@
+package day030;
+
+public class Kitap {
+    private boolean gecerliMi=true;
+    private String adi;
+    private String yazari;
+    private int sayfaAdedi;
+
+
+    public boolean isGecerliMi() {
+        return gecerliMi;
+    }
+
+    public void setGecerliMi(boolean gecerliMi) {
+        this.gecerliMi = gecerliMi;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        if (adi!=null&!adi.isEmpty()&!adi.isBlank()) {
+            this.adi = adi;
+        }else {
+            gecerliMi=false;
+            System.err.println("Kitap adı boş olamaz.");
+        }
+    }
+
+    public String getYazari() {
+        return yazari;
+    }
+
+    public void setYazari(String yazari) {
+        if (yazari!=null&!yazari.isEmpty()&!yazari.isBlank()) {
+            this.yazari = yazari;
+        }else {
+            gecerliMi=false;
+            System.err.println("Yazar adı boş olamaz.");
+        }
+    }
+
+    public int getSayfaAdedi() {
+        return sayfaAdedi;
+    }
+
+    public void setSayfaAdedi(int sayfaAdedi) {
+        if (sayfaAdedi>0) {
+            this.sayfaAdedi = sayfaAdedi;
+        }else {
+            gecerliMi=false;
+            System.err.println("Sayfa adedi negatif olamaz.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s : %d)",adi,yazari,sayfaAdedi);
+    }
+}
---
+++ src/day030/MainApp.java
@@ -0,0 +1,27 @@
+package day030;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Kitap kitap=new Kitap();
+        System.out.println("Kitap Giriş Ekranı");
+        Scanner input=new Scanner(System.in);
+        System.out.printf("Kitabın Adı : ");
+        kitap.setAdi(input.nextLine());
+        System.out.printf("Kitabın Yazarı : ");
+        kitap.setYazari(input.nextLine());
+        System.out.printf("Sayfa Adedi : ");
+        kitap.setSayfaAdedi(input.nextInt());
+
+        kaydet(kitap);
+    }
+    public static void kaydet(Kitap kitap){
+        if (kitap.isGecerliMi()) {
+            System.out.println(kitap + " veri tabanına kaydedildi.");
+        }else {
+            System.out.println("Kayıt işlemi başarısız.");
+        }
+    }
+}
