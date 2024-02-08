9f5ee1af52c13295c7695b55c2fa8d3bf19b1473
sinantalhaincesu <snn99tlh@hotmail.com>
2024-02-08 18:57:11 +0300

    Snn has been changed, day 35v2

---
+++ src/day035/Pencere/Main.java
@@ -0,0 +1,25 @@
+package day035.Pencere;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame pencere=new JFrame("Geometrik Şekiller V1.0");
+        pencere.setSize(800,600);
+
+        pencere.setLayout(null);
+
+        JButton button=new JButton("Tamam");
+        button.setSize(100,30);
+        button.setLocation(10,20);
+        pencere.add(button);
+
+        Kare kare1=new Kare(200);
+        kare1.setLocation(120,20);
+        pencere.add(kare1);
+
+        pencere.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pencere.setVisible(true);
+    }
+}
---
+++ src/day035/Pencere/Sekil.java
@@ -0,0 +1,6 @@
+package day035.Pencere;
+
+public interface Sekil {
+    int getAlan();
+    int getCevre();
+}
