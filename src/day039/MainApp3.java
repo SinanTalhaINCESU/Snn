0a3f43e477ef6eb7708fdd15d5a2e139fe34980b
sinantalhaincesu <snn99tlh@hotmail.com>
2024-02-13 23:25:56 +0300

    Snn has been changed, day 39v3

---
+++ src/day039/MainApp3.java
@@ -0,0 +1,35 @@
+package day039;
+
+import javax.swing.*;
+
+public class MainApp3 {
+    public static void main(String[] args) {
+
+        /*
+            Verilen ismin baş harflerini tutan, fakat geri kalan harflerin yerine asteriks(*) karakteri
+            koyan ve metni geriye döndüren bir metot yazın. Metodun ismi hideName olacaktır.
+            İsim klavyeden alınıp, ekranda örnekteki gibi gösterim yapılacaktır.
+            Örnek: Ahmet Dursun     A**** D*****
+         */
+
+        String isim= JOptionPane.showInputDialog(null,"İsminizi Giriniz:","İsim Gizleme",JOptionPane.PLAIN_MESSAGE);
+        System.out.println(hideNames(isim));
+    }
+
+    public static String hideNames(String name){
+        int indexSpace=name.indexOf(" ");
+        String tmp="";
+        for (int i = 0; i < name.length(); i++) {
+            if (i==0){
+                tmp+=name.charAt(i);
+            } else if (i==indexSpace+1) {
+                tmp+=name.charAt(indexSpace+1);
+            } else if (i==indexSpace) {
+                tmp+=" ";
+            }else {
+                tmp+="*";
+            }
+        }
+        return tmp;
+    }
+}
---
+++ src/day039/MainApp4.java
@@ -0,0 +1,17 @@
+package day039;
+
+import java.util.Arrays;
+
+public class MainApp4 {
+    public static void main(String[] args) {
+
+        String str="Ben bir garip keloğlanım.";
+        String[] kelimeler = str.split(" ");
+        System.out.println(Arrays.toString(kelimeler));
+        System.out.println("Dizinin Lengthi : "+kelimeler.length);
+        System.out.println();
+        for (String kelime:kelimeler) {
+            System.out.println(kelime);
+        }
+    }
+}
