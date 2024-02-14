bd479e2717669165886f8aa6e8ee1f4bb0c14cb6
sinantalhaincesu <snn99tlh@hotmail.com>
2024-02-14 19:56:45 +0300

    Snn has been changed, day 40v4

---
+++ src/day040/MainApp4.java
@@ -0,0 +1,24 @@
+package day040;
+
+public class MainApp4 {
+    public static void main(String[] args) {
+
+        StringBuilder sb=new StringBuilder("Merhaba!");
+
+        sb.append("Nasılsın?");
+        sb.insert(8,"Kardeş.");
+
+        System.out.println(sb);
+        System.out.println(sb.delete(8,14));
+        System.out.println(sb.reverse());
+
+        System.out.println(new StringBuilder("ey edip adanada pide yE").reverse());
+
+        System.out.println(
+                new StringBuilder()
+                        .append("Merhaba")
+                        .insert(0,"Ahmet")
+                        .reverse()
+        );
+    }
+}
