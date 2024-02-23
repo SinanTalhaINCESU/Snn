19d119e9823a0b0b1f170fb070b115b360c05df6
sinantalhaincesu <snn99tlh@hotmail.com>
2024-02-24 00:03:47 +0300

    Snn has been changed, day 41

--- src/day039/MainApp3.java
+++ src/day039/MainApp3.java
@@ -16,6 +16,7 @@ public class MainApp3 {
         System.out.println(hideNames(isim));
     }
 
+
     public static String hideNames(String name){
         int indexSpace=name.indexOf(" ");
         String tmp="";
